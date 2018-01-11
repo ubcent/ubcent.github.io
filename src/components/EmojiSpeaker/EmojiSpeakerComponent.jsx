98c818d8660ab0c37985b2b6b3985e6da2abf04e
dvbondarchuk@gmail.com <dvbondarchuk@gmail.com>
2018-01-12 00:17:44 +0300

    Start to make Emoji component

--- src/components/EmojiSpeaker/EmojiSpeakerComponent.jsx
+++ src/components/EmojiSpeaker/EmojiSpeakerComponent.jsx
@@ -15,12 +15,7 @@ emojis.forEach(emoji => emojiCode[emoji] = twemoji.parse(emoji, {
   ext: '.svg'
 }).slice('<img class="emoji" draggable="false" alt="😐" src="'.length, -3))
 console.log(emojiCode)
-function resolveCharacter(index) {
-  const character = message.toLowerCase()[index]
-  const previousDouble = message.toLowerCase().substr(index - 1, index + 1)
-  const nextDouble = message.toLowerCase().substr(index, index + 2)
-  return emojis.find(e => emojiMap[e].indexOf(previousDouble) !== -1) || emojis.find(e => emojiMap[e].indexOf(nextDouble) !== -1) || emojis.find(e => emojiMap[e].indexOf(character) !== -1)
-}
+
 setInterval(_ => {
   const index = Math.floor((Date.now() - start) / delay) % (message.length + 1)
   emojiIcon.src = emojiCode[resolveCharacter(index) || defaultEmoji]
@@ -41,7 +36,36 @@ const emojiMap = {
 
 class EmojiSpeaker extends PureComponent {
   static propTypes = {
+    message: PropTypes.string.isRequired,
+  }
 
+  constructor(props) {
+    super(props);
+
+    this.timer = null;
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      const index = Math.floor((Date.now() - start) / delay) % (message.length + 1)
+      emojiIcon.src = emojiCode[resolveCharacter(index) || defaultEmoji]
+      const words = message.substr(0, index).split(' ');
+      text.textContent = words.pop();
+      previousText.textContent = words.pop();
+    }, delay);
+  }
+
+  componentWillUnmount() {
+    if (this.timer) {
+      clearInterval(this.timer);
+    }
+  }
+
+  resolveCharacter = (index) => {
+    const character = message.toLowerCase()[index];
+    const previousDouble = message.toLowerCase().substr(index - 1, index + 1);
+    const nextDouble = message.toLowerCase().substr(index, index + 2);
+    return emojis.find(e => emojiMap[e].indexOf(previousDouble) !== -1) || emojis.find(e => emojiMap[e].indexOf(nextDouble) !== -1) || emojis.find(e => emojiMap[e].indexOf(character) !== -1)
   }
 }
 
